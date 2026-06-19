eed36214f67db6da1e861494e7a0acfd0074f4d6
Salvatore Bonavita <salvo.bonavita9808@gmail.com>
2026-06-19 17:05:05 +0200

    aggiornamento

--- data/athletes/antonio-cioppa.ts
+++ data/athletes/antonio-cioppa.ts
@@ -10,7 +10,7 @@ export const antonioCioppa: AthleteProfile = {
       "Antonio Cioppa (1999), playmaker 184 cm, Felice Scandone Avellino in Serie B Interregionale: cabina di regia, visione di gioco e percentuali al tiro. Statistiche, carriera e scheda scouting.",
     ogImage: "/athletes/cardcioppa7-og.jpg",
     ogImageVersion: "20260619-2",
-    publicSiteUrl: "https://antoniocioppa7.katahero.com",
+    publicSiteUrl: "https://antoniocioppa07.katahero.com",
   },
   legal: {
     dataController: {
